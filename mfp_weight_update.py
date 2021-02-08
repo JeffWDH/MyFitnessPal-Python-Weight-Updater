9b854b38df9e6d5e09513309cd20517d2c497a3c
JeffWDH <JeffWDH@users.noreply.github.com>
2021-02-08 08:16:45 -0500

    Added fake useragent to fix API login

--- mfp_weight_update.py
+++ mfp_weight_update.py
@@ -12,9 +12,14 @@ mfp_password = "Password"
 def update_mfp_weight(username:str, password:str, weight:float):
   # Create session to maintain cookies
   session = requests.Session()
+  useragent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.104 Safari/537.36"
+
+  headers = {
+    "User-Agent": useragent
+  }
 
   # Grab login page
-  mfpLoginPage = session.get("https://www.myfitnesspal.com/account/login")
+  mfpLoginPage = session.get("https://www.myfitnesspal.com/account/login", headers=headers)
 
   # Scrape authenticity_token
   parse = BeautifulSoup(mfpLoginPage.content, "html.parser")
@@ -28,7 +33,7 @@ def update_mfp_weight(username:str, password:str, weight:float):
   }
 
   # Submit login
-  session.post("https://www.myfitnesspal.com/account/login", data=post)
+  session.post("https://www.myfitnesspal.com/account/login", data=post, headers=headers)
 
   # Refresh API and grab data
   apijson = session.get("https://www.myfitnesspal.com/user/auth_token?refresh=true")
@@ -36,6 +41,7 @@ def update_mfp_weight(username:str, password:str, weight:float):
 
   # HTTP Headers
   headers = {
+    "User-Agent": useragent,
     'Accept': 'application/json',
     'Accept-Language': 'en-US,en;q=0.5',
     'Content-Type': 'application/json; charset=utf-8',
